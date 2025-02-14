4978a071363e351996ebdbd03cc0a42899908cfe
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-02-14 10:13:16 +0530

    Frontend created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Jayant Kumawat</h1>
+      <p><b>Contact:</b> jayant-email@gmail.com</p>
+      <p><b>Phone:</b> +91 9256XXXXXX</p>
+    </div>
+  );
+}
+
+export default App;
